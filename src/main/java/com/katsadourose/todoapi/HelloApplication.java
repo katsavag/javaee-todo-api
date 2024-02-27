c1cbd17b1a93b8240c116cc78c849f8a3e96349a
KatsadourosE <katsadourose@unisystems.gr>
2024-02-27 17:56:48 +0200

    first commit

---
+++ src/main/java/com/katsadourose/todoapi/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.katsadourose.todoapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class HelloApplication extends Application {
+
+}
